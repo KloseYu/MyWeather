ece714edb99a2b6d0eaa4f6045be511839c62c63
klose1178 <2836472769@qq.com>
2021-10-24 22:32:35 +0800

    插入注释

---
+++ app/src/main/java/com/myweather/android/logic/model/ServiceCreator.kt
@@ -0,0 +1,25 @@
+package com.myweather.android.logic.model
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * 文件名:ServiceCreator
+ * 作者: KloseYu
+ * 日期: 2021/10/24
+ * 描述: Retrofit构建器
+ */
+object ServiceCreator {
+
+    private  const  val BASE_URL ="http://api.caiyunapp.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun <T> create(serviceClass: Class<T>):T = retrofit.create(serviceClass)
+
+    inline fun <reified T> create():T = create(T::class.java)
+
+}
