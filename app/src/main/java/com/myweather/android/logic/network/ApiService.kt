3331a11481c51d63ed2d476a08f8bb680e6c0e8c
klose1178 <2836472769@qq.com>
2021-10-24 22:13:10 +0800

    插入注释

--- app/src/main/java/com/myweather/android/logic/network/ApiService.kt
+++ app/src/main/java/com/myweather/android/logic/network/ApiService.kt
@@ -6,7 +6,14 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+/**
+ * 文件名: ApiService
+ * 作者: KloseYu
+ * 日期: 2021/10/24
+ * 描述: Api接口
+ */
 interface ApiService {
+    //地区api
     @GET("2.5/place?${MyWeatherApplication.TOKEN}&lang=zh_CN")
     fun searchPlaces(@Query("query") query:String):Call<PlaceResponse>
 }
