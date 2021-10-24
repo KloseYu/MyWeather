712920f283a5c0a2e02ffec4d1709ffbb6d61207
klose1178 <2836472769@qq.com>
2021-10-24 22:07:55 +0800

    插入注释

--- app/src/main/java/com/myweather/android/MyWeatherApplication.kt
+++ app/src/main/java/com/myweather/android/MyWeatherApplication.kt
@@ -4,6 +4,14 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 
+/**
+ * 文件名: MyWeatherApplication
+ * 作者: KloseYu
+ * 日期: 2021/10/24
+ * 描述: Application
+ */
+
+
 class MyWeatherApplication : Application(){
     companion object{
         const val TOKEN ="auv03EiB3Zt9RZcn"
