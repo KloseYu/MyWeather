12a6d2b51705b0aa7fcb133a9d96f5cac67f7edc
klose1178 <2836472769@qq.com>
2021-11-03 22:17:15 +0800

    增加24小时天气

--- app/src/main/java/com/myweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/myweather/android/ui/weather/WeatherActivity.kt
@@ -164,9 +164,9 @@ class WeatherActivity : AppCompatActivity() {
             val temperature = daily.temperature[i]
             val view = LayoutInflater.from(this).inflate(R.layout.forecast_item, forecastLayout,false)
             val dateInfo = view.findViewById(R.id.dateInfo) as TextView
-            val skyIcon = view.findViewById(R.id.skyIcon) as ImageView
-            val skyInfo = view.findViewById(R.id.skyInfo) as TextView
-            val temperatureInfo = view.findViewById(R.id.temperatureInfo) as TextView
+            val skyIcon = view.findViewById(R.id.skyDailyIcon) as ImageView
+            val skyInfo = view.findViewById(R.id.skyDailyInfo) as TextView
+            val temperatureInfo = view.findViewById(R.id.temperatureDailyInfo) as TextView
             val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
             dateInfo.text = simpleDateFormat.format(skycon.date)
             val sky = getSky(skycon.value)
