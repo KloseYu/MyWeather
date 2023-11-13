5cb30e6bb371aa65afd13ccdd557388c1aa3aade
KloseYu <62323013+klose1178@users.noreply.github.com>
2023-11-13 16:19:14 +0800

    更新 ApiService.kt
    Api更新：2023/10/13

--- app/src/main/java/com/myweather/android/logic/network/ApiService.kt
+++ app/src/main/java/com/myweather/android/logic/network/ApiService.kt
@@ -15,23 +15,25 @@ import retrofit2.http.Query
  * 作者: KloseYu
  * 日期: 2021/10/24
  * 描述: Api接口
+ * Api更新：2023/11/13
  */
+
 interface ApiService {
     //全球地区api
-    @GET("v2/place?${TOKEN}&lang=zh_CN")
+    @GET("v2.6/place?${TOKEN}&lang=zh_CN")
     fun searchPlaces(@Query("query") query:String):Call<PlaceResponse>
 
     //实时天气api
-    @GET("v2.5/${TOKEN}/{lng},{lat}/realtime.json")
+    @GET("v2.6/${TOKEN}/{lng},{lat}/realtime.json")
     fun getRealWeather(@Path("lng") lng:String,@Path("lat") lat:String):Call<RealtimeResponse>
 
 
     //24小时天气api
-    @GET("v2.5/${TOKEN}/{lng},{lat}/hourly.json?hourlysteps=24")
+    @GET("v2.6/${TOKEN}/{lng},{lat}/hourly.json?hourlysteps=24")
     fun getHourlyWeather(@Path("lng") lng:String,@Path("lat") lat:String):Call<HourlyResponse>
 
 
     //最近天气api
-    @GET("v2.5/${TOKEN}/{lng},{lat}/daily.json")
+   @GET("v2.6/${TOKEN}/{lng},{lat}/daily.json")
     fun getDailyWeather(@Path("lng") lng:String,@Path("lat") lat:String):Call<DailyResponse>
 }
