8f4840f33ec91052ed35bcfce0a1f104581e4e97
KloseYu <62323013+klose1178@users.noreply.github.com>
2023-11-14 08:32:14 +0800

    更新 MyWeatherApplication.kt

--- app/src/main/java/com/myweather/android/MyWeatherApplication.kt
+++ app/src/main/java/com/myweather/android/MyWeatherApplication.kt
@@ -4,12 +4,13 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 
-/**
+/*
  * 文件名: MyWeatherApplication
  * 作者: KloseYu
  * 日期: 2021/10/24
  * 描述: Application
  */
+
 class MyWeatherApplication : Application(){
     companion object{
         const val TOKEN ="8yTp8hXFWAxo0n5C"
