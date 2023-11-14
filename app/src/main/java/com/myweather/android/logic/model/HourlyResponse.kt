c6cadbe936a7a82ac6c04973ba2da41e32b290b0
KloseYu <62323013+klose1178@users.noreply.github.com>
2023-11-14 08:28:34 +0800

    更新 HourlyResponse.kt

--- app/src/main/java/com/myweather/android/logic/model/HourlyResponse.kt
+++ app/src/main/java/com/myweather/android/logic/model/HourlyResponse.kt
@@ -1,12 +1,13 @@
 package com.myweather.android.logic.model
 
 
-/**
+/*
  * 文件名: HourlyResponse
  * 作者: KloseYu
  * 日期: 2021/11/03
  * 描述: 24小时天气模块
  */
+
 data class HourlyResponse(
     val status:String,
     val result:Result,
