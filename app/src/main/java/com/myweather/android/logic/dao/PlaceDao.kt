1e6a26a152949939fed27df494d47013639a197b
KloseYu <62323013+klose1178@users.noreply.github.com>
2023-11-14 08:27:48 +0800

    更新 PlaceDao.kt

--- app/src/main/java/com/myweather/android/logic/dao/PlaceDao.kt
+++ app/src/main/java/com/myweather/android/logic/dao/PlaceDao.kt
@@ -6,12 +6,13 @@ import androidx.core.content.edit
 import com.myweather.android.MyWeatherApplication
 import com.myweather.android.logic.model.PlaceResponse
 
-/**
+/*
  * 文件名: PlaceDao
  * 作者: KloseYu
  * 日期: 2021/10/27
  * 描述: 存储sharedpreferences
  */
+
 object PlaceDao {
     fun savePlace(place: PlaceResponse.Place){
         sharedPreferences().edit(){
