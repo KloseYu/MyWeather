8d9c75f1ad68ff9a1e8cf100bd8f17fdf1de26fb
KloseYu <62323013+klose1178@users.noreply.github.com>
2023-11-14 08:29:06 +0800

    更新 RealtimeResponse.kt

--- app/src/main/java/com/myweather/android/logic/model/RealtimeResponse.kt
+++ app/src/main/java/com/myweather/android/logic/model/RealtimeResponse.kt
@@ -2,12 +2,13 @@ package com.myweather.android.logic.model
 
 import com.google.gson.annotations.SerializedName
 
-/**
+/*
  * 文件名: RealtimeResponse
  * 作者: KloseYu
  * 日期: 2021/10/27
  * 描述: 实时天气模块
  */
+
 data class RealtimeResponse (
     val status:String,
     val result:Result
